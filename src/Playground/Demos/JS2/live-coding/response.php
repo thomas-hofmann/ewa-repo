5fc5404d95bfa4e0e2cb3de4f734cad2b7316314
Thomas Hofmann <thomas.hofmann@h-da.de>
2024-04-30 11:36:25 +0200

    Deleted all demos updated gitignore

--- src/Playground/Demos/JS2/live-coding/response.php
+++
@@ -1,6 +0,0 @@
-<?php
-$myArray = ['name' => 'Thomas', 'age' => 35];
-$myJsonString = json_encode($myArray);
-
-header('Content-Type: application/json');
-echo $myJsonString;
--- src/Playground/Demos/PHP2/session-destroy.php
+++
@@ -1,5 +0,0 @@
-<?php
-// session muss immer gestartet werden
-session_start();
-session_destroy();
-header('Location: session-output.php');
